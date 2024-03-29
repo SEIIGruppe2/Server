e24fb0eca55cd88234cae194c872aa26fc3c303a
paulxyh <77801088+paulxyh@users.noreply.github.com>
2024-03-29 16:08:31 +0100

    feat: ModelTests
    RunAllTests des Models enabled

---
+++ src/test/java/at/aau/se2/model/ModelTests.java
@@ -0,0 +1,10 @@
+package at.aau.se2.model;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("at.aau.se2.model")
+public class ModelTests {
+}
