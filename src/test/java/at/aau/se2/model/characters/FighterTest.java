9b4215d4b8620c9484483c9c357408be585821e8
paulxyh <77801088+paulxyh@users.noreply.github.com>
2024-03-29 16:09:12 +0100

    feat: Model of Characters Testing
    * Testing für alle Klassen im subpackage .characters mit 100% Coverage implementiert

---
+++ src/test/java/at/aau/se2/model/characters/FighterTest.java
@@ -0,0 +1,35 @@
+package at.aau.se2.model.characters;
+
+import at.aau.se2.model.Monster;
+import at.aau.se2.model.monsters.Bullrog;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FighterTest {
+    Fighter fighter;
+    Monster monster;
+    @BeforeEach
+    public void setupTests(){
+        fighter = new Fighter(2);
+        monster = new Bullrog(2, 3);
+    }
+
+    @Test
+    public void testSetup(){
+        assertEquals("Schwertkämpfer", fighter.getName());
+        assertEquals(2, fighter.getZone());
+        assertEquals(1, fighter.getDmg());
+    }
+
+    @Test
+    public void testDoesDmg(){
+        assertEquals(0, fighter.doesDmg(monster));
+        monster.setRing(2);
+        assertEquals(-1, fighter.doesDmg(monster));
+        monster.setZone(1);
+        assertEquals(-1, fighter.doesDmg(monster));
+
+    }
+}
