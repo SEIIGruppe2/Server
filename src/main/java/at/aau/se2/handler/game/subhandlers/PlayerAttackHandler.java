efb1b9195065e5532664698fac61cbc1477e7cb8
paulxyh <77801088+paulxyh@users.noreply.github.com>
2024-04-06 18:10:35 +0200

    fix: PlayerAttack handleMessage updated

--- src/main/java/at/aau/se2/handler/game/subhandlers/PlayerAttackHandler.java
+++ src/main/java/at/aau/se2/handler/game/subhandlers/PlayerAttackHandler.java
@@ -1,10 +1,13 @@
 package at.aau.se2.handler.game.subhandlers;
 
 import at.aau.se2.exceptions.PlayerNotFoundException;
-import at.aau.se2.gameutils.Lobby;
-import at.aau.se2.gameutils.Player;
-import at.aau.se2.gameutils.UtilityMethods;
-import at.aau.se2.model.Monster;
+import at.aau.se2.model.Actioncard;
+import at.aau.se2.model.characters.Archer;
+import at.aau.se2.model.characters.Fighter;
+import at.aau.se2.model.characters.Hero;
+import at.aau.se2.model.characters.Knight;
+import at.aau.se2.utils.Lobby;
+import at.aau.se2.utils.UtilityMethods;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.web.socket.WebSocketSession;
 
@@ -14,13 +17,19 @@ public class PlayerAttackHandler implements ActionHandler {
     public void handleMessage(WebSocketSession session, JsonNode msg, Lobby lobby){
         String[] m = readInfosFromMessage(msg);
         try{
-            UtilityMethods.findPlayer(session, lobby)
-                    .getCards()
-                    .get(Integer.parseInt(m[1]))
-                    .doesDmg(lobby
-                            .getGameState()
-                            .getMonsters()
-                            .get(Integer.parseInt(m[0])));
+            Actioncard card = UtilityMethods
+                                .findPlayer(session, lobby)
+                                .getCards()
+                                .get(Integer.parseInt(m[1]));
+            card.doesDmg(lobby.getGameState().getMonsters().get(Integer.parseInt(m[0])));
+            if(card instanceof Archer)
+                lobby.getGameState().decreaseCardAmount(0);
+            else if(card instanceof Fighter)
+                lobby.getGameState().decreaseCardAmount(1);
+            else if(card instanceof Knight)
+                lobby.getGameState().decreaseCardAmount(2);
+            else if(card instanceof Hero)
+                lobby.getGameState().decreaseCardAmount(3);
         }
         catch(PlayerNotFoundException p){
             System.out.println(p.getMessage());
