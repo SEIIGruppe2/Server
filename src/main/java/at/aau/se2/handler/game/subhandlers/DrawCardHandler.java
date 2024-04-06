1cef09414bb07b161b03238e0a4bbb3ad80d63b2
paulxyh <77801088+paulxyh@users.noreply.github.com>
2024-04-06 18:08:54 +0200

    fix: fixed drawRandomCard & handleMessage

--- src/main/java/at/aau/se2/handler/game/subhandlers/DrawCardHandler.java
+++ src/main/java/at/aau/se2/handler/game/subhandlers/DrawCardHandler.java
@@ -1,8 +1,8 @@
 package at.aau.se2.handler.game.subhandlers;
 
 import at.aau.se2.exceptions.PlayerNotFoundException;
-import at.aau.se2.gameutils.Lobby;
-import at.aau.se2.gameutils.UtilityMethods;
+import at.aau.se2.utils.Lobby;
+import at.aau.se2.utils.UtilityMethods;
 import at.aau.se2.model.Actioncard;
 import at.aau.se2.model.characters.Archer;
 import at.aau.se2.model.characters.Fighter;
@@ -25,8 +25,9 @@ public class DrawCardHandler implements ActionHandler {
     public void handleMessage(WebSocketSession session, JsonNode msg, Lobby lobby){
         // generate a new card and send it to client
         try {
-            UtilityMethods.findPlayer(session, lobby).getCards().add(drawRandomCard(lobby));
-            session.sendMessage(new TextMessage(convertCardToJson(drawRandomCard(lobby))));
+            Actioncard card = drawRandomCard(lobby);
+            UtilityMethods.findPlayer(session, lobby).getCards().add(card);
+            session.sendMessage(new TextMessage(convertToJson(card)));
         }
         catch(PlayerNotFoundException p){
             System.out.println(p.getMessage() + "PLAYER NOT IN LOBBY");
@@ -36,9 +37,13 @@ public class DrawCardHandler implements ActionHandler {
         }
     }
 
-    public String convertCardToJson(Actioncard card){
-        return "{ 'type' : 'DRAW_CARD', 'name': '" +
+    public String convertToJson(Actioncard card){
+        return "{ 'type' : 'DRAW_CARD', 'id': '" +
+                card.getId() +
+                "', 'name': '" +
                 card.getName() +
+                "', 'zone': '" +
+                card.getZone() +
                 "'}";
     }
 
@@ -46,9 +51,9 @@ public class DrawCardHandler implements ActionHandler {
         SecureRandom rn = new SecureRandom();
         int i = lobby.getGameState().getIndexMinimumCardAmount();
         Actioncard card = switch(i){
-            case 1 -> new Archer(rn.nextInt(1,4), cardId++);
-            case 2 -> new Fighter(rn.nextInt(1,4), cardId++);
-            case 3 -> new Knight(rn.nextInt(1,4), cardId++);
+            case 0 -> new Archer(rn.nextInt(1,4), cardId++);
+            case 1 -> new Fighter(rn.nextInt(1,4), cardId++);
+            case 2 -> new Knight(rn.nextInt(1,4), cardId++);
             default -> new Hero(rn.nextInt(1,4), cardId++);
         };
         lobby.getGameState().increaseCardAmount(i);
