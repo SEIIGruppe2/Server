a966a3a32d188ec95ebd0261c1f0dea00b04650f
paulxyh <77801088+paulxyh@users.noreply.github.com>
2024-04-06 13:53:01 +0200

    feat: implemented handling of PlayerAttack

--- src/main/java/at/aau/se2/handler/game/subhandlers/PlayerAttackHandler.java
+++ src/main/java/at/aau/se2/handler/game/subhandlers/PlayerAttackHandler.java
@@ -1,6 +1,10 @@
 package at.aau.se2.handler.game.subhandlers;
 
+import at.aau.se2.exceptions.PlayerNotFoundException;
 import at.aau.se2.gameutils.Lobby;
+import at.aau.se2.gameutils.Player;
+import at.aau.se2.gameutils.UtilityMethods;
+import at.aau.se2.model.Monster;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.web.socket.WebSocketSession;
 
@@ -8,9 +12,26 @@ import org.springframework.web.socket.WebSocketSession;
 public class PlayerAttackHandler implements ActionHandler {
     @Override
     public void handleMessage(WebSocketSession session, JsonNode msg, Lobby lobby){
+        String[] m = readInfosFromMessage(msg);
+        try{
+            UtilityMethods.findPlayer(session, lobby)
+                    .getCards()
+                    .get(Integer.parseInt(m[1]))
+                    .doesDmg(lobby
+                            .getGameState()
+                            .getMonsters()
+                            .get(Integer.parseInt(m[0])));
+        }
+        catch(PlayerNotFoundException p){
+            System.out.println(p.getMessage());
+        }
+    }
+    public String[] readInfosFromMessage(JsonNode msg){
+        String[] arr = new String[2];
+        arr[0] = msg.path("monsterid").asText();
+        arr[1] = msg.path("cardid").asText();
+        return arr;
+    }
 
-    }
-    public String[][] readInfosFromMessage(JsonNode msg){
-        return new String[0][0];
-    }
+
 }
