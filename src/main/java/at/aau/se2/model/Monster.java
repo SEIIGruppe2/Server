ba7b11c6c83a5fe737a058392dc87fc1a4b945eb
paulxyh <77801088+paulxyh@users.noreply.github.com>
2024-04-06 13:51:11 +0200

    feat: convertToJson Method added

--- src/main/java/at/aau/se2/model/Monster.java
+++ src/main/java/at/aau/se2/model/Monster.java
@@ -4,16 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 @Getter
 public abstract class Monster extends Tower{
-    protected final int id;
     protected String name;
     @Setter
     protected int zone;
     @Setter
     protected int ring;
     public Monster(int zone, int ring, int id){
+        super(id);
         this.zone = zone % 4; // 0-3
         this.ring = ring % 4; // 0-3
-        this.id = id;
     }
     public int doesDmg(Tower tower){
         if(tower.lifepoints >= 1) {
@@ -21,5 +20,25 @@ public abstract class Monster extends Tower{
             return 0;
         }
         return -1;
+    }
+    @Override
+    public String convertToJson(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("{ 'id': '")
+                .append(this.id)
+                .append("', ")
+                .append("'zone': '")
+                .append(this.zone)
+                .append("', ")
+                .append("'ring': '")
+                .append(this.ring)
+                .append("', ")
+                .append("'name': '")
+                .append(this.name)
+                .append("', ")
+                .append("'lifepoints': '")
+                .append(this.lifepoints)
+                .append("'}");
+        return builder.toString();
     }
 }
--- src/main/java/at/aau/se2/model/Tower.java
+++ src/main/java/at/aau/se2/model/Tower.java
@@ -4,9 +4,24 @@ import lombok.Getter;
 
 @Getter
 public abstract class Tower {
+    protected final int id;
     protected int lifepoints;
+    public Tower(int id){
+        this.id = id;
+    }
     public int takeDamage(int dmg){
         this.lifepoints -= dmg;
         return (lifepoints > 0) ? 0 : -1;
+    }
+
+    public String convertToJson(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("{ 'id': '")
+                .append(this.id)
+                .append("',")
+                .append("'lifepoints':'")
+                .append(this.lifepoints)
+                .append("'}");
+        return builder.toString();
     }
 }
