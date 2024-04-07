598a5641beeab176c39023acb0a9b2dd833aa55c
paulxyh <77801088+paulxyh@users.noreply.github.com>
2024-04-07 12:26:28 +0200

    feat: UnitTests Lobby CodeCleanup

--- src/main/java/at/aau/se2/utils/Lobby.java
+++ src/main/java/at/aau/se2/utils/Lobby.java
@@ -1,14 +1,13 @@
 package at.aau.se2.utils;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 public class Lobby {
-    private List<Player> players;
+    private final List<Player> players;
     private final GameState gameState;
     public Lobby(GameState gameState){
         this.gameState = gameState;
