fcd4a7412e69833099e0f08fbcbac5e9a5018729
paulxyh <77801088+paulxyh@users.noreply.github.com>
2024-04-07 15:44:45 +0200

    feat: Added package wide test runs

---
+++ src/test/java/at/aau/se2/UtilsTests.java
@@ -0,0 +1,9 @@
+package at.aau.se2;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+import org.junit.platform.runner.JUnitPlatform;
+@RunWith(JUnitPlatform.class)
+@SelectPackages("at.aau.se2.utils")
+public class UtilsTests {
+}
