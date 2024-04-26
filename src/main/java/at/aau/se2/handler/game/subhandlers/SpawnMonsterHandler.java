bcd2e386bf05a6b193a0a9d683701a395bbd3842
Manuel1598 <manuel.weilguni1@gmail.com>
2024-04-26 14:10:14 +0200

    feat: Implementation and customisation of the SpawnMonsterHandler class

--- src/main/java/at/aau/se2/handler/game/subhandlers/SpawnMonsterHandler.java
+++ src/main/java/at/aau/se2/handler/game/subhandlers/SpawnMonsterHandler.java
@@ -1,12 +1,51 @@
 package at.aau.se2.handler.game.subhandlers;
 
+import at.aau.se2.model.Monster;
+import at.aau.se2.model.monsters.Bullrog;
+import at.aau.se2.model.monsters.Slime;
+import at.aau.se2.model.monsters.Sphinx;
 import at.aau.se2.utils.Lobby;
 import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
+import java.security.SecureRandom;
+import java.util.logging.Logger;
 
-public class SpawnMonsterHandler implements ActionHandler{
+
+
+public class SpawnMonsterHandler implements ActionHandler {
+    private final SecureRandom rn;
+    public static int monsterId = 0;
+
+    public SpawnMonsterHandler(){
+        rn = new SecureRandom();
+    }
     @Override
     public void handleMessage(WebSocketSession session, JsonNode msg, Lobby lobby) {
+        try {
+            int zone = msg.path("zone").asInt();
+            Logger.getLogger("global").info("Received zone: " + zone);
+            Monster monster = spawnRandomMonster(zone,lobby);
+            session.sendMessage(new TextMessage(convertToJson(monster)));
+        }catch (Exception e){
+            Logger.getLogger("global")
+                    .info("SPAWN_MONSTER: " + e.getMessage());
+        }
+    }
 
+    private Monster spawnRandomMonster(int zone,Lobby lobby){
+        int monstertype = rn.nextInt(1,3);
+
+        Monster monster = switch(monstertype){
+            case 1 -> new Slime(zone, 0, monsterId++);
+            case 2 -> new Sphinx(zone, 0, monsterId++);
+            default -> new Bullrog(zone, 0, monsterId++);
+        };
+        lobby.getGameState().getMonsters().add(monster);
+        return monster;
+    }
+
+    public String convertToJson(Monster monster) {
+        return "{ 'type':'SPAWN_MONSTER', 'monster': " + monster.convertToJson() + "}";
     }
 }
