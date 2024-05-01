b6632df9514405ca4d6100687894532e8f3af12e
TimonWoelbitsch <100867226+TimonWoelbitsch@users.noreply.github.com>
2024-05-01 16:49:24 +0200

    implemented testcases for switchCardDeckHandler

---
+++ src/test/java/at/aau/se2/handler/dummy/SwitchCardDeckHandlerTest.java
@@ -0,0 +1,60 @@
+package at.aau.se2.handler.dummy;
+import at.aau.se2.handler.game.subhandlers.SwitchCardDeckHandler;
+import at.aau.se2.model.characters.Knight;
+import at.aau.se2.utils.GameState;
+import at.aau.se2.utils.Lobby;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.minidev.json.JSONObject;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class SwitchCardDeckHandlerTest {
+    private Lobby lobby;
+    private SwitchCardDeckHandler test;
+    private SecureRandom rn;
+
+    @BeforeEach
+    public void setup(){
+        lobby = mock(Lobby.class);
+        when(lobby.getGameState()).thenReturn(new GameState());
+        rn = mock(SecureRandom.class);
+        test = new SwitchCardDeckHandler();
+    }
+
+    @Test
+    public void testconvertToJson(){
+
+        Knight actioncard = new Knight(2,5);
+        String json= "{'type':'SWITCH_CARD_DECK_RESPONSE', "+
+                actioncard.convertToJson() + "}";;
+        assertEquals(json,test.convertToJson(actioncard));
+    }
+
+
+    @Test
+    public void readInfosFromMessage(){
+       Knight actioncard = new Knight(1, 23);
+        JSONObject json = new JSONObject();
+        json.put("cardid",String.valueOf(actioncard.getId()));
+        json.put("type","SWITCH_CARD_DECK");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode node = mapper.valueToTree(json);
+        String jsonmsg = "{ 'type':'SWITCH_CARD_DECK', 'id': '"+actioncard.getId()+"'}";
+        String[] result = new String[2];
+        result[0] = "SWITCH_CARD_DECK";
+        result[1] = "23";
+
+        Assert.assertArrayEquals(result,test.readInfosFromMessage(node));
+
+    }
+
+
+}
