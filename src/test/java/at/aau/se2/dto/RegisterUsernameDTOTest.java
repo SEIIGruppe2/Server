4dc1b59f7e42b644a8a2c8fd813d69ae339d34bd
paulxyh <77801088+paulxyh@users.noreply.github.com>
2024-05-08 14:00:38 +0200

    feat: implemented RegisterUsernameDTOTest

--- src/test/java/at/aau/se2/dto/RegisterUsernameDTOTest.java
+++ src/test/java/at/aau/se2/dto/RegisterUsernameDTOTest.java
@@ -1,5 +1,22 @@
 package at.aau.se2.dto;
 
-public class RegisterUsernameDTOTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.web.socket.TextMessage;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RegisterUsernameDTOTest {
+    private RegisterUsernameDTO dto;
+
+    @Test
+    public void testMakeMessage(){
+        dto = new RegisterUsernameDTO("accepted");
+        String msg = "{\"type\":\"REGISTER_USERNAME\",\"response\":\"accepted\"}";
+        TextMessage exp = new TextMessage(msg);
+        assertEquals(exp, dto.makeMessage());
+        msg = "{\"type\":\"REGISTER_USERNAME\",\"response\":\"denied\"}";
+        exp = new TextMessage(msg);
+        dto = new RegisterUsernameDTO("denied");
+        assertEquals(exp, dto.makeMessage());
+    }
 }
