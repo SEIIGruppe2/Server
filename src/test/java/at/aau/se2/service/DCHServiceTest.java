3fd94d69f8d28bca246dbbf0f6464143f1fc0967
paulxyh <77801088+paulxyh@users.noreply.github.com>
2024-05-08 13:36:17 +0200

    feat: Implemented DCHService and corresponding testcases

---
+++ src/test/java/at/aau/se2/service/DCHServiceTest.java
@@ -0,0 +1,54 @@
+package at.aau.se2.service;
+
+import at.aau.se2.model.characters.Archer;
+import at.aau.se2.model.characters.Fighter;
+import at.aau.se2.model.characters.Hero;
+import at.aau.se2.model.characters.Knight;
+import at.aau.se2.utils.GameState;
+import at.aau.se2.utils.Lobby;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.security.SecureRandom;
+
+import static at.aau.se2.service.DCHService.convertToJson;
+import static at.aau.se2.service.DCHService.drawRandomCard;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+
+public class DCHServiceTest {
+    private Lobby lobby;
+    private SecureRandom rn;
+    private GameState gs;
+    private DCHService dchs;
+
+    @BeforeEach
+    public void setup(){
+        this.lobby = mock(Lobby.class);
+        this.rn = mock(SecureRandom.class);
+        this.gs = mock(GameState.class);
+        this.dchs = new DCHService(rn);
+    }
+
+    @Test
+    public void testDrawRandomCard(){
+        when(lobby.getGameState()).thenReturn(gs);
+        when(gs.getIndexMinimumCardAmount()).thenReturn(0);
+        when(rn.nextInt(anyInt(), anyInt())).thenReturn(1);
+        assertEquals(Archer.class, drawRandomCard(lobby).getClass());
+        when(gs.getIndexMinimumCardAmount()).thenReturn(1);
+        assertEquals(Fighter.class, drawRandomCard(lobby).getClass());
+        when(gs.getIndexMinimumCardAmount()).thenReturn(2);
+        assertEquals(Knight.class, drawRandomCard(lobby).getClass());
+        when(gs.getIndexMinimumCardAmount()).thenReturn(3);
+        assertEquals(Hero.class, drawRandomCard(lobby).getClass());
+        verify(gs, times(4)).getIndexMinimumCardAmount();
+    }
+
+    @Test
+    public void testConvertToJson(){
+        String exp = "{ 'type' : 'DRAW_CARD', 'id': '1', 'name': 'Bogenschütze', 'zone': '1'}";
+        assertEquals(exp, convertToJson(new Archer(1,1)));
+    }
+}
