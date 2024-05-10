ca9032be8c8403cb92170370c2186d89abca6578
Yvonne Egger <y1egger@edu.aau.at>
2024-05-10 14:38:43 +0200

    feat: Updated MonsterAttackHandler
    Files modified:
    - Modified MonsterAttackHandler.java

--- src/main/java/at/aau/se2/handler/game/subhandlers/MonsterAttackHandler.java
+++ src/main/java/at/aau/se2/handler/game/subhandlers/MonsterAttackHandler.java
@@ -1,5 +1,6 @@
 package at.aau.se2.handler.game.subhandlers;
 
+import at.aau.se2.dto.MonsterAttackDTO;
 import at.aau.se2.utils.Lobby;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.web.socket.WebSocketSession;
@@ -23,3 +24,26 @@ public class MonsterAttackHandler implements ActionHandler {
 
                 // Trigger the monster attack (this is a static call)
                 triggerMonsterAttack(monsterId, towerId, lobby);
+
+                // Retrieve monster and tower data from the lobby to construct the DTO
+                var monster = lobby.getGameState().getMonsters().get(Integer.parseInt(monsterId));
+                var tower = lobby.getGameState().getTowers().get(Integer.parseInt(towerId));
+
+                // Create the MonsterAttackDTO based on the results of the attack
+                String attackStatus = (monster.getLifepoints() > 0) ? "success" : "failed";
+                MonsterAttackDTO dto = new MonsterAttackDTO(
+                        monsterId,
+                        monster.getLifepoints(),
+                        tower.getLifepoints(),
+                        attackStatus
+                );
+
+                // Send the DTO as a message to the client
+                session.sendMessage(dto.makeMessage());
+            }
+
+        } catch (Exception e) {
+            logger.severe("Error processing MONSTER_ATTACK message: " + e.getMessage());
+        }
+    }
+}
