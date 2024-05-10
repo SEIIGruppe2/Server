36f3b2dfb3817ef3de3d68a25c80827f37a3f131
Renato Pliso <renatopl@edu.aau.at>
2024-05-10 14:05:34 +0200

    feat: Added Constructor to MonsterAttackDTO

--- src/main/java/at/aau/se2/dto/MonsterAttackDTO.java
+++ src/main/java/at/aau/se2/dto/MonsterAttackDTO.java
@@ -1,4 +1,16 @@
 package at.aau.se2.dto;
 
-public class MonsterAttackDTO {
+public class MonsterAttackDTO extends ParentDTO {
+    private final String monsterId;
+    private final int monsterHp;
+    private final int towerHp;
+    private final String attackStatus;
+
+    public MonsterAttackDTO(String monsterId, int monsterHp, int towerHp, String attackStatus) {
+        super("MONSTER_ATTACK");
+        this.monsterId = monsterId;
+        this.monsterHp = monsterHp;
+        this.towerHp = towerHp;
+        this.attackStatus = attackStatus;
+    }
 }
