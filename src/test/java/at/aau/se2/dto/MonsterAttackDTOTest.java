542b3d90c9a8b15ef4042afac27af186ad907d62
Yvonne Egger <y1egger@edu.aau.at>
2024-05-10 15:21:52 +0200

    refactor: MonsterAttackDTOTest set access modifier to protected

--- src/test/java/at/aau/se2/dto/MonsterAttackDTOTest.java
+++ src/test/java/at/aau/se2/dto/MonsterAttackDTOTest.java
@@ -6,16 +6,16 @@ import org.springframework.web.socket.TextMessage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MonsterAttackDTOTest {
+class MonsterAttackDTOTest {
     private MonsterAttackDTO dto;
 
     @BeforeEach
-    public void setup() {
+    protected void setup() {
         dto = new MonsterAttackDTO("monster1", 10, 5, "success");
     }
 
     @Test
-    public void testMakeMessage() {
+    protected void testMakeMessage() {
         String expectedJson = "{\"type\":\"MONSTER_ATTACK\",\"monsterId\":\"monster1\",\"monsterHp\":10,\"towerHp\":5,\"attackStatus\":\"success\"}";
         TextMessage expectedMessage = new TextMessage(expectedJson);
         assertEquals(expectedMessage, dto.makeMessage());
