c9195e75992a4048b7f139443461bd76b16bddd6
Renato Pliso <renatopl@edu.aau.at>
2024-05-10 14:36:42 +0200

    feat: Added handleMessage Functionality

--- src/main/java/at/aau/se2/handler/game/subhandlers/MonsterAttackHandler.java
+++ src/main/java/at/aau/se2/handler/game/subhandlers/MonsterAttackHandler.java
@@ -4,13 +4,22 @@ import at.aau.se2.utils.Lobby;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.util.logging.Logger;
+
+import static at.aau.se2.service.MAHService.triggerMonsterAttack;
+
 public class MonsterAttackHandler implements ActionHandler {
-    public String[][] readInfosFromMessage(JsonNode msg){
-        return new String[0][0];
-    }
+    private static final Logger logger = Logger.getLogger(MonsterAttackHandler.class.getName());
 
     @Override
     public void handleMessage(WebSocketSession session, JsonNode msg, Lobby lobby) {
+        try {
+            String messageType = msg.path("type").asText();
 
-    }
-}
+            if ("MONSTER_ATTACK".equals(messageType)) {
+                // Extract monster and tower IDs from the message
+                String monsterId = msg.path("monsterid").asText();
+                String towerId = msg.path("towerid").asText();
+
+                // Trigger the monster attack (this is a static call)
+                triggerMonsterAttack(monsterId, towerId, lobby);
