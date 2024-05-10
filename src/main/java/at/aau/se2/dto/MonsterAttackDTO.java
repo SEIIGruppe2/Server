5c4be2ea0d78de612a66c3d5ab805e839d9d3e9d
Yvonne Egger <y1egger@edu.aau.at>
2024-05-10 14:09:53 +0200

    feat: Override abstract method in MonsterAttackDTO
    Files modified:
    - Modified MonsterAttackDTO.java

--- src/main/java/at/aau/se2/dto/MonsterAttackDTO.java
+++ src/main/java/at/aau/se2/dto/MonsterAttackDTO.java
@@ -1,5 +1,8 @@
 package at.aau.se2.dto;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.web.socket.TextMessage;
+
 public class MonsterAttackDTO extends ParentDTO {
     private final String monsterId;
     private final int monsterHp;
@@ -12,5 +15,16 @@ public class MonsterAttackDTO extends ParentDTO {
         this.monsterHp = monsterHp;
         this.towerHp = towerHp;
         this.attackStatus = attackStatus;
+    }
+
+    @Override
+    public TextMessage makeMessage() {
+        ObjectNode node = this.getMapper().createObjectNode();
+        node.put("type", this.getType());
+        node.put("monsterId", monsterId);
+        node.put("monsterHp", monsterHp);
+        node.put("towerHp", towerHp);
+        node.put("attackStatus", attackStatus);
+        return new TextMessage(node.toString());
     }
 }
