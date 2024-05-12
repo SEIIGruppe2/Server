9ab7b79c1188c57a7ed2e5246ecc66a2f4a73e23
Renato Pliso <renatopl@edu.aau.at>
2024-05-12 22:22:27 +0200

    refactor: Moved several helper methods from MonsterAttackHandler to MAHService
    Modified file: MAHService

--- src/main/java/at/aau/se2/service/MAHService.java
+++ src/main/java/at/aau/se2/service/MAHService.java
@@ -1,16 +1,33 @@
+// File path: at/aau/se2/service/MAHService.java
+
 package at.aau.se2.service;
 
+import at.aau.se2.utils.Lobby;
+import com.fasterxml.jackson.databind.JsonNode;
 import at.aau.se2.model.Monster;
 import at.aau.se2.model.Tower;
-import at.aau.se2.utils.Lobby;
 
 import java.util.List;
 
-
-import static at.aau.se2.utils.UtilityMethods.*;
+import static at.aau.se2.utils.UtilityMethods.logs;
 
 public class MAHService {
 
+    public static boolean isMessageTypeValid(JsonNode msg) {
+        return "MONSTER_ATTACK".equals(msg.path("type").asText());
+    }
+
+    public static boolean validateEntities(String monsterId, String towerId, Lobby lobby) {
+        try {
+            int mId = Integer.parseInt(monsterId);
+            int tId = Integer.parseInt(towerId);
+            return (mId >= 0 && mId <= lobby.getGameState().getMonsters().size()) &&
+                    (tId >= 0 && tId <= lobby.getGameState().getTowers().size());
+        } catch (NumberFormatException e) {
+            logs("Invalid number format for entity IDs. " + e.getMessage());
+            return false;
+        }
+    }
 
     public static void triggerMonsterAttack(String monsterId, String towerId, Lobby lobby) {
         try {
@@ -21,10 +38,8 @@ public class MAHService {
                 int attackResult = monster.doesDmg(tower);
                 if (attackResult == 0) {
                     monster.takeDamage(1);
-
-                    logi("Monster " + monsterId + " attacked Tower " + towerId + " and booth took 1 damage.\nTower HP: " + tower.getLifepoints() + ",\nMonster HP: " + monster.getLifepoints());
                 } else {
-                    logd("Tower could not be damaged. No damage taken by Monster.");
+                    logs("Tower could not be damaged. No damage taken by Monster.");
                 }
             } else {
                 logs("Invalid MonsterID or invalid TowerID.");
@@ -33,7 +48,8 @@ public class MAHService {
             logs("Error parsing IDs: " + e.getMessage());
         }
     }
-    // Helper methods to find a monster or tower by ID from a list
+
+    // Helper methods to find monsters or towers by ID from a list
     static Monster findMonsterById(List<Monster> monsters, int id) {
         return monsters.stream().filter(m -> m.getId() == id).findFirst().orElse(null);
     }
