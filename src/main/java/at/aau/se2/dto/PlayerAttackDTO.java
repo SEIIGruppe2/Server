7ef674037a0150beef1cddc17b546da5c16613db
paulxyh <77801088+paulxyh@users.noreply.github.com>
2024-05-27 19:45:28 +0200

    refactor: started refactoring PlayerAttack

---
+++ src/main/java/at/aau/se2/dto/PlayerAttackDTO.java
@@ -0,0 +1,23 @@
+package at.aau.se2.dto;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.web.socket.TextMessage;
+
+public class PlayerAttackDTO extends ParentDTO{
+    private final String monsterId;
+    private final int lp;
+    public PlayerAttackDTO(String type, String monsterId, int lp) {
+        super("CARD_ATTACK_MONSTER");
+        this.monsterId = monsterId;
+        this.lp = lp;
+    }
+
+    @Override
+    public TextMessage makeMessage() {
+        ObjectNode node = this.getMapper().createObjectNode();
+        node.put("type", this.getType());
+        node.put("monsterId", this.monsterId);
+        node.put("lifepoints", this.lp);
+        return new TextMessage(node.toString());
+    }
+}
---
+++ src/main/java/at/aau/se2/service/PAHService.java
@@ -0,0 +1,11 @@
+package at.aau.se2.service;
+
+import at.aau.se2.model.Actioncard;
+
+import java.util.List;
+
+public class PAHService {
+    public static void removeCard(int id, List<Actioncard> cards){
+
+    }
+}
