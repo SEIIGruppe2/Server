d3359fd126d6757f0922dcca3f1bfc5809a5cef1
paulxyh <77801088+paulxyh@users.noreply.github.com>
2024-06-02 11:47:02 +0200

    refactor: deleted mapping of dummyHandler

--- src/main/java/at/aau/se2/handler/WebSocketConfig.java
+++ src/main/java/at/aau/se2/handler/WebSocketConfig.java
@@ -11,4 +11,6 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry reg){
-        reg.addHandler(GameHandler.getInstance()
+        reg.addHandler(GameHandler.getInstance(), "/game").setAllowedOrigins("*");
+    }
+}
