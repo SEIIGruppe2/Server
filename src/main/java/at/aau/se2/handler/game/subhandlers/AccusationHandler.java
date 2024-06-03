f82dd1c78137b243543f278a0cbd385c3f13ece5
Yvonne Egger <y1egger@edu.aau.at>
2024-06-03 21:22:59 +0200

    feat: updated AccusationHandler to work probably

--- src/main/java/at/aau/se2/handler/game/subhandlers/AccusationHandler.java
+++ src/main/java/at/aau/se2/handler/game/subhandlers/AccusationHandler.java
@@ -1,6 +1,7 @@
 package at.aau.se2.handler.game.subhandlers;
 
 import at.aau.se2.dto.PlayerTrophiesDTO;
+import at.aau.se2.exceptions.PlayerNotFoundException;
 import at.aau.se2.utils.Lobby;
 import at.aau.se2.utils.Player;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -8,6 +9,7 @@ import org.springframework.web.socket.WebSocketSession;
 
 import static at.aau.se2.service.ACCService.checkCheater;
 import static at.aau.se2.service.SCPHService.findPlayerByUsername;
+import static at.aau.se2.utils.UtilityMethods.logs;
 
 public class AccusationHandler implements ActionHandler {
     @Override
@@ -24,3 +26,22 @@ public class AccusationHandler implements ActionHandler {
                     cheaterName,
                     cheater.getPoints()
             );
+            PlayerTrophiesDTO accusatorDTO = new PlayerTrophiesDTO(
+                    accusatorName,
+                    accusator.getPoints()
+            );
+            for (Player players : lobby.getPlayers()) {
+                try {
+                    synchronized (players.getSession()) {
+                        players.getSession().sendMessage(cheaterDTO.makeMessage());
+                        players.getSession().sendMessage(accusatorDTO.makeMessage());
+                    }
+                } catch (Exception e) {
+                    logs("Failed to send message to player " + players.getUsername() + ": " + e.getMessage());
+                }
+            }
+        } catch (PlayerNotFoundException e) {
+            logs("Error processing ACCUSE message: " + e.getMessage());
+        }
+    }
+}
