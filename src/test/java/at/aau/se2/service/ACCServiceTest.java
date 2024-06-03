ae6eedcc4cb134dc012de7a9f7bd69be5b241bb9
Yvonne Egger <y1egger@edu.aau.at>
2024-06-03 21:26:46 +0200

    feat: updated ACCServiceTest with addition test cases

--- src/test/java/at/aau/se2/service/ACCServiceTest.java
+++ src/test/java/at/aau/se2/service/ACCServiceTest.java
@@ -19,6 +19,31 @@ public class ACCServiceTest {
     public void setUp() {
         WebSocketSession session = mock(WebSocketSession.class);
         Lobby lobby = mock(Lobby.class);
-        cheater = new Player (session, lobby);
+        cheater = new Player(session, lobby);
         accusator = new Player(session, lobby);
     }
+
+    @Test
+    public void testCheckCheaterWhenCheating() {
+        cheater.setPoints(10);
+        cheater.setCheating(true);
+        cheater.setCheatingRoundsLeft(3);
+
+        ACCService.checkCheater(cheater, accusator);
+
+        assertEquals(0, cheater.getPoints());
+        assertEquals(0, cheater.getCheatingRoundsLeft());
+        assertFalse(cheater.isCheating());
+    }
+
+    @Test
+    public void testCheckCheaterWhenNotCheating() {
+        accusator.setPoints(10);
+        cheater.setCheating(false);
+
+        ACCService.checkCheater(cheater, accusator);
+
+        assertEquals(0, accusator.getPoints());
+    }
+}
+
