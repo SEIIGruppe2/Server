9a990dc82b1f675c8ce256961ef9195008ec5f73
Yvonne Egger <y1egger@edu.aau.at>
2024-06-12 14:19:11 +0200

    docs: Added JavaDocs to PTService class and updatePlayerPoints method
    Provided detailed documentation for the PTService class, including the updatePlayerPoints method. The JavaDocs describe the class's purpose and explain the method's parameters and functionality.

--- src/main/java/at/aau/se2/service/PTService.java
+++ src/main/java/at/aau/se2/service/PTService.java
@@ -1,10 +1,23 @@
 package at.aau.se2.service;
+
 import at.aau.se2.utils.Player;
 
 import static at.aau.se2.utils.UtilityMethods.logi;
 import static at.aau.se2.utils.UtilityMethods.logs;
 
+/**
+ * Service class for updating player points based on the monster defeated.
+ * This class provides a method to update the player's points according to the monster's name.
+ */
 public class PTService {
+
+    /**
+     * Updates the points of the given player based on the name of the monster defeated.
+     * Different monsters provide different point values.
+     *
+     * @param player the player whose points will be updated
+     * @param monsterName the name of the monster defeated
+     */
     public static void updatePlayerPoints(Player player, String monsterName) {
         int pointsToAdd = 0;
         switch (monsterName) {
