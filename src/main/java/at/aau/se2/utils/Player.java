c2ce6d66d066061ebfbc0de53bd3032510610436
paulxyh <77801088+paulxyh@users.noreply.github.com>
2024-06-12 10:21:28 +0200

    docs: generated JavaDoc for the utils package

--- src/main/java/at/aau/se2/utils/Player.java
+++ src/main/java/at/aau/se2/utils/Player.java
@@ -8,6 +8,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+/**
+ * The Player class represents a player in the game.
+ * It contains information about the player, such as username, points, and cards.
+ */
 public class Player {
     @Getter
     private final Lobby lobby;
@@ -29,6 +33,14 @@ public class Player {
 
     @Getter
     private final List<Actioncard> cards;
+
+    /**
+     * Constructs a new Player object with the given WebSocket session and Lobby.
+     * Initializes player attributes including username, player ID, session, lobby, points, cards, and cheating status.
+     *
+     * @param session the WebSocket session associated with the player
+     * @param lobby the Lobby object to which the player belongs
+     */
     public Player(WebSocketSession session, Lobby lobby){
         this.username = "";
         this.usernameSet = false;
@@ -42,10 +54,21 @@ public class Player {
         this.isCheating = false;
     }
 
+    /**
+     * Sets the points of the player by adding the specified points to the current points.
+     *
+     * @param points the points to be added to the player's current points
+     */
     public void setPoints(int points){
         this.points += points;
     }
 
+    /**
+     * Sets the username of the player if it has not already been set.
+     * Logs a message if the username has already been set.
+     *
+     * @param username the username to be set for the player
+     */
     public void setUsername(String username){
         if(!usernameSet){
             this.username = username;
