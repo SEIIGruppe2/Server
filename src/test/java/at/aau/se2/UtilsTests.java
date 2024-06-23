ddf355376a8c33882c88b092a1c1a470dc742f67
paulxyh <77801088+paulxyh@users.noreply.github.com>
2024-06-23 16:46:58 +0200

    fix: SonarCloud issue fix
    * removed unnecessary testfiles

--- src/test/java/at/aau/se2/UtilsTests.java
+++
@@ -1,9 +0,0 @@
-package at.aau.se2;
-
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.runner.RunWith;
-import org.junit.platform.runner.JUnitPlatform;
-@RunWith(JUnitPlatform.class)
-@SelectPackages("at.aau.se2.utils")
-public class UtilsTests {
-}
