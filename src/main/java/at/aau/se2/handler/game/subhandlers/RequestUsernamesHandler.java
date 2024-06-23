06d474f5158fbe62910c7368e85cd8983d9c3f98
paulxyh <77801088+paulxyh@users.noreply.github.com>
2024-06-23 17:22:03 +0200

    fix: SonarCloud issue fix
    * removed TODOs

--- src/main/java/at/aau/se2/handler/game/subhandlers/RequestUsernamesHandler.java
+++ src/main/java/at/aau/se2/handler/game/subhandlers/RequestUsernamesHandler.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.util.logging.Logger;
 
 public class RequestUsernamesHandler implements ActionHandler, JsonSerializable {
-    // TODO: DTO
     @Override
     public void handleMessage(WebSocketSession session, JsonNode msg, Lobby lobby) {
         try{
